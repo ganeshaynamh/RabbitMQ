276e3343cdbee661162d395dfe6ce5f792958777
Reeta-7 <49265323+Reeta-7@users.noreply.github.com>
2019-08-21 14:40:35 +0530

    generate Exchange

---
+++ Third_exchange_ReceiveMSg/Program.cs
@@ -0,0 +1,61 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Third_exchange_ReceiveMSg
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Exchange Receiving Message");
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(exchange: "logs", type: "fanout");
+
+                    //declaring Queue
+                    var queue = channel.QueueDeclare().QueueName;
+
+                    //binding the queue
+                    channel.QueueBind(queue: queue, exchange: "logs", routingKey: "");
+
+                    // pritnting the waiting logs
+
+                    Console.WriteLine("[*] waiting logs ");
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var getmessage = Encoding.UTF8.GetString(body);
+
+                        Console.WriteLine("[x] received message {0}", getmessage);
+
+
+                    };
+                    channel.BasicConsume(queue: queue,
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to close the windows.");
+                    Console.ReadLine();
+
+
+                }
+            }
+            catch (Exception)
+            {
+                //
+            }
+
+
+        }
+    }
+}
---
+++ Third_exchange_SendingMsg/Program.cs
@@ -0,0 +1,51 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Third_exchange_SendingMsg
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Sending Exchange Message");
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(exchange: "logs", type: "fanout");
+
+                    var message = GetMessage(args);
+                    var Convert_message = Encoding.UTF8.GetBytes(message.ToString());
+
+                    channel.BasicPublish(exchange: "logs",
+                        routingKey: "",
+                        basicProperties: null,
+                        body: Convert_message
+                        );
+                    Console.WriteLine("[x] sent message is {0}", message);
+
+                }
+
+                Console.WriteLine("presss enter to close the Window");
+                Console.ReadLine();
+
+            }
+            catch (Exception)
+            {
+                ///s
+            }
+        }
+
+        private static object GetMessage(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                return string.Join(" ", args);
+            }
+            return "hello jagdish this message is empty";
+        }
+    }
+}
